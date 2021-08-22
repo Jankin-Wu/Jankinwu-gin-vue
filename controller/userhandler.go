20a893fd73f1c6b71a2bb0d4269191b17cf10bad
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2021-08-22 13:07:27 +0800

    go-gin-vue后台

--- controller/userhandler.go
+++ controller/userhandler.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"Jankinwu-gin-vue/util"
 	"github.com/gin-gonic/gin"
 	"github.com/shuwenhe/utils"
 	"net/http"
@@ -18,6 +19,15 @@ func Register(c *gin.Context) {
 			"code": 422,
 			"msg":  "The phone num must be 11 digits!",
 		})
+	}
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "The password must be greater than 6 digits!",
+		})
+	}
+	if len(name) == 0 {
+		name = util.RandomString(10)
 	}
 	c.JSON(utils.NewSucc("Register success!", gin.H{
 		"msg": "Register success!",
